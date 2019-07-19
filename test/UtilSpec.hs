96acb0b90662589f62d5956b18eb2d5a4e9d58e1
Gary Coady <gcoady@earnestresearch.com>
2019-07-19 17:01:47 -0400

    Fix tests

--- test/UtilSpec.hs
+++ test/UtilSpec.hs
@@ -9,6 +9,6 @@ import Test.Hspec.QuickCheck
 spec :: Spec
 spec = do
   describe "plus2" $ do
-    it "basic check" $ plus2 0 `shouldBe` 3
+    it "basic check" $ plus2 0 `shouldBe` 2
     it "overflow" $ plus2 maxBound `shouldBe` minBound + 1
     prop "minus 2" $ \i -> plus2 i - 2 `shouldBe` i
