d14f7eb08410f0e7e91f32a8b8d7b43377c6a79c
Gary Coady <gcoady@earnestresearch.com>
2019-08-30 13:31:16 +0200

    Add required nix file

---
+++ honeycomb.nix
@@ -0,0 +1,17 @@
+{ mkDerivation, aeson, base, bytestring, hashable, http-client
+, http-client-tls, http-types, microlens, microlens-mtl, mtl
+, network-uri, random, resourcet, scientific, stdenv, stm, text
+, time, unliftio, unordered-containers, uuid
+}:
+mkDerivation {
+  pname = "honeycomb";
+  version = "0.1.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    aeson base bytestring hashable http-client http-client-tls
+    http-types microlens microlens-mtl mtl network-uri random resourcet
+    scientific stm text time unliftio unordered-containers uuid
+  ];
+  homepage = "https://github.com/EarnestResearch/honeycomb#readme";
+  license = stdenv.lib.licenses.bsd3;
+}
