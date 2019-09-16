2f14ecfc6383d9eb59e01d964ab23506de71c839
Gary Coady <gcoady@earnestresearch.com>
2019-09-16 16:09:55 +0200

    Revert "No longer an exception to attempt to send event with key/dataset unset"
    This reverts commit f64304db4876077548ac75f05d2fa805e6b34d0b.

--- src/Honeycomb.hs
+++ src/Honeycomb.hs
@@ -159,13 +159,14 @@ send'
 send' extraFields event = do
     let shouldSample = True
     apiEvent <- toApiEvent
-    maybe (pure ()) (\opts -> sendApiEvent shouldSample opts apiEvent) toRequestOptions
+    requestOpts <- toRequestOptions
+    sendApiEvent shouldSample requestOpts apiEvent
   where
-    toRequestOptions :: Maybe Api.RequestOptions
-    toRequestOptions =
-        Api.mkRequestOptions (event ^. eventOptionsL . apiHostL)
-            <$> event ^. eventOptionsL . datasetL
-            <*> event ^. eventOptionsL . apiKeyL
+    toRequestOptions :: m Api.RequestOptions
+    toRequestOptions = do
+        ds <- maybe (throwIO MissingDatasetOption) pure $ event ^. eventOptionsL . datasetL
+        key <- maybe (throwIO MissingApiKeyOption) pure $ event ^. eventOptionsL . apiKeyL
+        pure $ Api.mkRequestOptions (event ^. eventOptionsL . apiHostL) ds key
 
     toApiEvent :: m Api.Event
     toApiEvent = do
