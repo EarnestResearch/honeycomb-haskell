56ba05d7944c933879bb1c76f494b8d5174a6a65
Gary Coady <gcoady@earnestresearch.com>
2020-01-21 19:35:41 +0100

    Add serialized plan (must be changed if cabal plan changes)

--- default.nix
+++ default.nix
@@ -5,6 +5,8 @@ pkgs.haskell-nix.cabalProject {
   src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
   ghc = pkgs.haskell-nix.compiler.${ghc};
   index-state = "2020-01-15T00:00:00Z";
-  # update if build changes
+
+  # update everything below if cabal plan changes
   plan-sha256 = "1gs3wnj91ck4wx41fqx55pljv2s2rh2wa338cz6lbz9fwl88n9gn";
+  materialized = ./materialized;
 }
