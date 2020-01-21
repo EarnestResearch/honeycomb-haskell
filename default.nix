45ecb5f6db5c6eb6a646656df7cc4cee6b713c09
Gary Coady <gcoady@earnestresearch.com>
2020-01-21 19:31:42 +0100

    Set plan-sha256 (faster nix builds)

--- default.nix
+++ default.nix
@@ -5,4 +5,6 @@ pkgs.haskell-nix.cabalProject {
   src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
   ghc = pkgs.haskell-nix.compiler.${ghc};
   index-state = "2020-01-15T00:00:00Z";
+  # update if build changes
+  plan-sha256 = "1gs3wnj91ck4wx41fqx55pljv2s2rh2wa338cz6lbz9fwl88n9gn";
 }
