d884b27e32483412e749321196fad4accddff524
Gary Coady <gcoady@earnestresearch.com>
2020-01-21 19:28:28 +0100

    Update index-state and make ghc version configurable in nix

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 { pkgs ? import ./nixpkgs {}
+, ghc ? "ghc865"
 }:
 pkgs.haskell-nix.cabalProject {
   src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
-  ghc = pkgs.haskell-nix.compiler.ghc865;
-  index-state = "2019-10-20T00:00:00Z";
+  ghc = pkgs.haskell-nix.compiler.${ghc};
+  index-state = "2020-01-15T00:00:00Z";
 }
